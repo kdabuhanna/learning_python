3c6ed596337e439550b5205d3cccfbec9b5f3919
kdabuhanna <kdabuhanna@ucdavis.edu>
2021-05-20 10:22:10 -0700

    _dictionary

--- mcb185.py
+++ mcb185.py
@@ -1,4 +1,5 @@
 import random
+import sys
 
 def read_fasta(filename):
 	name = None
@@ -41,4 +42,32 @@ def randseq(length, gc):
 		else: seq += random.choice('AT')
 	return seq
 
+gcode = {
+		'AAA' : 'K',	'AAC' : 'N',	'AAG' : 'K',	'AAT' : 'N',
+		'ACA' : 'T',	'ACC' : 'T',	'ACG' : 'T',	'ACT' : 'T',
+		'AGA' : 'R',	'AGC' : 'S',	'AGG' : 'R',	'AGT' : 'S',
+		'ATA' : 'I',	'ATC' : 'I',	'ATG' : 'M',	'ATT' : 'I',
+		'CAA' : 'Q',	'CAC' : 'H',	'CAG' : 'Q',	'CAT' : 'H',
+		'CCA' : 'P',	'CCC' : 'P',	'CCG' : 'P',	'CCT' : 'P',
+		'CGA' : 'R',	'CGC' : 'R',	'CGG' : 'R',	'CGT' : 'R',
+		'CTA' : 'L',	'CTC' : 'L',	'CTG' : 'L',	'CTT' : 'L',
+		'GAA' : 'E',	'GAC' : 'D',	'GAG' : 'E',	'GAT' : 'D',
+		'GCA' : 'A',	'GCC' : 'A',	'GCG' : 'A',	'GCT' : 'A',
+		'GGA' : 'G',	'GGC' : 'G',	'GGG' : 'G',	'GGT' : 'G',
+		'GTA' : 'V',	'GTC' : 'V',	'GTG' : 'V',	'GTT' : 'V',
+		'TAA' : '*',	'TAC' : 'Y',	'TAG' : '*',	'TAT' : 'Y',
+		'TCA' : 'S',	'TCC' : 'S',	'TCG' : 'S',	'TCT' : 'S',
+		'TGA' : '*',	'TGC' : 'C',	'TGG' : 'W',	'TGT' : 'C',
+		'TTA' : 'L',	'TTC' : 'F',	'TTG' : 'L',	'TTT' : 'F',
+	}
 
+def translate(seq):
+	for seq in sys.argv[1:]:
+		prot = ''
+		for i in range(0, len(seq)-2, 3):
+			codon = seq[i:i+3]
+			if codon in gcode:
+				prot += gcode[codon]
+			else:
+				prot = 'X'
+		return prot
